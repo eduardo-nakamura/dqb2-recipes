c59fa65aef42eff014080cd8ada696d2892cd488
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-01-21 17:28:21 -0300

    aca

--- src/app/recipes/room-recipe/room-recipe.component.ts
+++ src/app/recipes/room-recipe/room-recipe.component.ts
@@ -1,35 +1,25 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-
-export interface RoomRecipeList {
-  name: string;
-  number: string;  
-  content: string;
-  roomType: string;
-}
-
-const ROOM_DATA: RoomRecipeList[] = [
-  {number: '001', name: 'Basic Bedroom', content:'Any Bed x 2, Any Light Source x 1',roomType:'Bedroom' },
-  {number: '014', name: 'Double Bathroom', content:'Bathtubs/Showers x2, Towel Rack x2, Washtubs x2, Light Source x1',roomType:'Bathroom' },
-];
-
+import * as tables from './roomObj';
 @Component({
   selector: 'app-room-recipe',
   templateUrl: './room-recipe.component.html',
   styleUrls: ['./room-recipe.component.scss']
 })
 export class RoomRecipeComponent implements OnInit {
-  displayedColumns: string[] = ['number', 'name', 'content', 'roomType'];
-  bedroomTable = new MatTableDataSource(ROOM_DATA);
+  displayedColumns: string[] = ['number', 'name', 'shortDesc', 'roomType'];
+  bedroomTable = new MatTableDataSource(tables.ROOM_DATA);
   list: any[];
+  btnFilter =['Bedroom',
+    'Bathroom','Kitchen','Private Bedroom','Bar','Toilet','Changing','Workshop','Shop','Miscelaneous','Museum','More Miscelaneous','Special'
+  ] 
   // @ViewChild(MatSort, {static: true}) sort: MatSort;
   ngOnInit() {
-    console.log(this.bedroomTable)
+    console.log(tables.ROOM_DATA)    
     // this.bedroomTable.sort = this.sort;
   }
   applyFilter(filterValue: string) {    
-    console.log(this.bedroomTable)
     this.bedroomTable.filter = filterValue.trim().toLowerCase();
   }
 }
